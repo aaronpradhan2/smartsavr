dfd3b34ef9a709044d10e6440db91b85042ed926
Parth-Khaladkar <parthkhaladkar224@gmail.com>
2023-04-06 14:57:54 -0400

    Added in User Model

--- app/src/main/java/com/example/smartsavr/MainActivity.java
+++ app/src/main/java/com/example/smartsavr/MainActivity.java
@@ -17,11 +17,13 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
     ProgressDialog progressDialog;
     @Override
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         firebaseAuth= FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
         progressDialog = new ProgressDialog(this);
 
 
@@ -48,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
                             public void onSuccess(AuthResult authResult) {
                                 startActivity(new Intent(MainActivity.this,LoginActivity.class));
                                 progressDialog.cancel();
+
+                                firebaseFirestore.collection("User")
+                                        .document(FirebaseAuth.getInstance().getUid());
 
                             }
                         })
