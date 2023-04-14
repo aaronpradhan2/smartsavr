293870fc4bd68ce6fd472dc9230245d7310c5d75
Aaron Pradhan <12a.pradhan@gmail.com>
2023-04-13 20:58:51 -0400

    Add link to add a child screen

--- app/src/main/java/com/example/smartsavr/Parent_HomeScreenZero.java
+++ app/src/main/java/com/example/smartsavr/Parent_HomeScreenZero.java
@@ -2,6 +2,7 @@ package com.example.smartsavr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Parent_HomeScreenZero extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class Parent_HomeScreenZero extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_home_screen_zero);
+
+        findViewById(R.id.add_a_child_text_view).setOnClickListener(view -> startActivity(new Intent(this, AddChildActivity.class)));
     }
 }
